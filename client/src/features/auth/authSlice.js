b244209ba10b5dec59fcceb08d2dc4142371b26d
forrestmorrison <forrestmorrison@gmail.com>
2023-01-04 12:43:40 -0600

    created authService

--- client/src/features/auth/authSlice.js
+++ client/src/features/auth/authSlice.js
@@ -1,4 +1,5 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import authService from './authService'
 
 // Get user from localStorage
 const user = JSON.parse(localStorage.getItem('user'))
@@ -11,11 +12,20 @@ const initialState = {
     message: ''
 }
 
+// Register user
+export const register = createAsyncThunk('auth/register', async (user, thunkAPI) => {
+    try {
+        return await authService.register(user)
+    } catch (error) {
+
+    }
+})
+
 export const authSlice = createSlice({
     name: 'auth',
     initialState,
     reducers: {
-        reset: () => {
+        reset: (state) => {
             state.isLoading = false
             state.isSuccess = false
             state.isError = false
