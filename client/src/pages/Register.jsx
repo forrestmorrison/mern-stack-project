00a8a5a68eb7d9c39ce189a3dfafad720ec4519a
forrestmorrison <forrestmorrison@gmail.com>
2023-01-03 19:20:59 -0600

    added create account form to Register page

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,8 +1,80 @@
-
+import { useState, useEffect } from "react"
+import { FaUser } from "react-icons/fa"
 
 const Register = () => {
+  
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
+  })
+
+  const { name, email, password, password2 } = formData
+
+  const onChange = () => {}
+
   return (
-    <div>Register</div>
+    <>
+      <section className="heading">
+        <h1>
+          <FaUser /> Register
+        </h1>
+        <p>Please create an account</p>
+      </section>
+
+      <section className="form">
+        <form>
+          <div className="form-group">
+            <input 
+              type="text" 
+              className="form-control" 
+              id="name" 
+              name="name" 
+              value={name} 
+              placeholder="enter your name"
+              onChange={onChange} 
+            />
+          </div>
+          <div className="form-group">
+            <input 
+              type="text" 
+              className="form-control" 
+              id="email" 
+              name="email" 
+              value={email} 
+              placeholder="enter your email"
+              onChange={onChange} 
+            />
+          </div>
+          <div className="form-group">
+            <input 
+              type="text" 
+              className="form-control" 
+              id="password" 
+              name="password" 
+              value={password} 
+              placeholder="enter your password"
+              onChange={onChange} 
+            />
+          </div>
+          <div className="form-group">
+            <input 
+              type="text" 
+              className="form-control" 
+              id="password2" 
+              name="password2" 
+              value={password2} 
+              placeholder="re-enter your password"
+              onChange={onChange} 
+            />
+          </div>
+          <div className="form-group">
+            <button type="submit" className="btn btn-block">Submit</button>
+          </div>
+        </form>
+      </section>
+    </>
   )
 }
 
