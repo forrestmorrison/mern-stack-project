3c010ae8afc6e41929d97065aa729b4c791174c9
forrestmorrison <forrestmorrison@gmail.com>
2023-01-05 16:49:09 -0600

    added content section to Dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -5,6 +5,7 @@ import { useSelector, useDispatch } from "react-redux"
 import { getGoals, reset } from "../features/goals/goalSlice"
 
 import GoalForm from "../components/GoalForm"
+import GoalItem from "../components/GoalItem"
 import Spinner from "../components/Spinner"
 
 
@@ -42,7 +43,20 @@ const Dashboard = () => {
         <h1>Welcome {user && user.name}</h1>
         <p>Goals Dashboard</p>
       </section>
+
       <GoalForm />
+
+      <section className='content'>
+        {goals.length > 0 ? (
+          <div className='goals'>
+            {goals.map((goal) => (
+              <GoalItem key={goal._id} goal={goal} />
+            ))}
+          </div>
+        ) : (
+          <h3>You have not set any goals</h3>
+        )}
+      </section>
     </>
   )
 }
