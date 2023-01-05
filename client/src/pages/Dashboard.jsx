d50413a5b8aec21c7bc94a8c76a7f6c5c7015a9c
forrestmorrison <forrestmorrison@gmail.com>
2023-01-05 15:06:55 -0600

    added GoalForm

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,6 +2,8 @@ import { useEffect } from "react"
 import { useNavigate } from "react-router-dom"
 import { useSelector } from "react-redux"
 
+import GoalForm from "../components/GoalForm"
+
 
 const Dashboard = () => {
   
@@ -16,7 +18,13 @@ const Dashboard = () => {
   }, [user, navigate])
 
   return (
-    <div>Dashboard</div>
+    <>
+      <section className="heading">
+        <h1>Welcome {user && user.name}</h1>
+        <p>Goals Dashboard</p>
+      </section>
+      <GoalForm />
+    </>
   )
 }
 
